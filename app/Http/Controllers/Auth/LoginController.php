aa16aabde340ce5554fd4bbe635094599bd1e294
rvillalobos <rvillalobos@ittepic.edu.mx>
2019-06-03 16:28:00 -0600

    Inicio de proyecto, ya tiene la conexion a la base de datos y el login funciona en su primera etapa

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Auth;
+use App\User;
+use App\Http\Controllers\Controller;
+
+class LoginController extends Controller
+{
+
+    public function __construct(){
+        $this->middleware('guest', ['only' => 'showLoginForm']);
+    }
+    public function showLoginForm(){
+        return view('inicio');
+    }
+    public function login(){
+        $credenciales = $this->validate(request(), [
+            $this->username() => 'email|required|string',
+            'password' => 'required|string'
+        ]);
+        /*$Username = request(['email']);
+        $dbUsername = User::where("email","=", $Username)->first();
+        dd($dbUsername);*/
+        if (Auth::attempt($credenciales)){
+            return redirect()->route('dashboard');
+        }
+        return back()->withErrors([$this->username() => 'Estas credenciales no coinciden'])->withInput(request([$this->username()]));
+    }
+
+    public function logout(){
+        Auth::logout();
+        return redirect('/');
+    }
+
+    public function username(){
+        return 'email';
+    }
+}
